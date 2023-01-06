ade06082543abe4294fb4c1b26a1e91101d5fc8e
Cliff L. Biffle <code@cliffle.com>
2023-01-06 09:48:53 -0800

    dynamic::decompress: rearrange to avoid unsafe
    This is a little more subtle -- wound up rearranging how the index
    masking is handled to make sure the compiler can always see that the
    indices are in range.

--- lzss/src/dynamic/decompress.rs
+++ lzss/src/dynamic/decompress.rs
@@ -12,16 +12,19 @@ impl LzssDyn {
     writer: &mut W,
     buffer: &mut [u8],
   ) -> Result<(), LzssError<R::Error, W::Error>> {
+    let n = 1 << self.ei;
+    let index_mask = n - 1;
+    let buffer = &mut buffer[..n];
     let mut bit_reader = BitReader::new(reader);
 
-    let mut r = self.n() - self.f();
+    let mut r = n - self.f();
     loop {
       if let Some(c) = bit_reader.read_bits(1).map_err(LzssError::ReadError)? {
         if c != 0 {
           if let Some(b) = bit_reader.read_bits(8).map_err(LzssError::ReadError)? {
             writer.write(b as u8).map_err(LzssError::WriteError)?;
-            *unsafe { buffer.get_unchecked_mut(r) } = b as u8;
-            r = (r + 1) & (self.n() - 1);
+            buffer[r & index_mask] = b as u8;
+            r = r.wrapping_add(1);
           } else {
             return Ok(());
           }
@@ -32,10 +35,10 @@ impl LzssDyn {
           let i = (ij >> self.ej) as usize;
           let j = (ij & ((1 << self.ej) - 1)) as usize;
           for k in 0..=j + self.p() {
-            let b = *unsafe { buffer.get_unchecked((i + k) & (self.n() - 1)) };
+            let b = buffer[i.wrapping_add(k) & index_mask];
             writer.write(b).map_err(LzssError::WriteError)?;
-            *unsafe { buffer.get_unchecked_mut(r) } = b;
-            r = (r + 1) & (self.n() - 1);
+            buffer[r & index_mask] = b;
+            r = r.wrapping_add(1);
           }
         } else {
           return Ok(());
